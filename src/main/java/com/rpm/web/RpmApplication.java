ae454ca70d81b551964fd17c9f25bce160cc517b
h32109 <h32109@gmail.com>
2020-02-05 15:02:26 +0900

    start
    start

--- src/main/java/com/rpm/web/RpmApplication.java
+++ src/main/java/com/rpm/web/RpmApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RpmApplication {
 
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(RpmApplication.class, args);
     }
 }
