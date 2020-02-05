4c37efdde079d33a34432389d212967de67a17f3
h32109 <h32109@gmail.com>
2020-02-05 21:41:24 +0900

    commit
    commit

--- src/main/java/com/rpm/web/RpmApplication.java
+++ src/main/java/com/rpm/web/RpmApplication.java
@@ -1,10 +1,14 @@
 package com.rpm.web;
 
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableBatchProcessing
 public class RpmApplication {
 
     public static void main(String[] args) throws Exception {
